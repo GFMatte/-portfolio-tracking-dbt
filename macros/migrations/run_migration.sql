eb29aa5acd9f3a19ba738bd653de211c57efe0f6
Guilherme Matte <guilhermematte93@gmail.com>
2025-11-24 11:24:52 +0000

    run_migration.sql macro defined

---
+++ macros/migrations/run_migration.sql
@@ -0,0 +1,16 @@
+{% macro run_migrations(
+            database = target.database,
+            schema_prefix = target.schema
+) -%}
+
+{% do log(
+    "Running V001_drop_example_table migration with database = " ~ database ~ ", schema_prefix = " ~ schema_prefix,
+    info=True) %}
+
+{% do run_query(V001_drop_example_table(
+                        database, schema_prefix)) %}    
+
+{#% do log("No migration to run.", info=True) %#}                        
+-- Remove # to uncomment if no migration to run
+
+{-% endmacro %}

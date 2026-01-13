6af1615d7dfbeed6dd748e5d10c0b49f5aa5bd5e
Guilherme Matte <guilhermematte93@gmail.com>
2026-01-13 13:23:05 +0000

    Changed SNSH with HIST table for ABC_BANK_SECURIT_INFO

--- models/refined/REF_ABC_BANK_SECURITY_INFO.sql
+++ models/refined/REF_ABC_BANK_SECURITY_INFO.sql
@@ -1,8 +1,12 @@
-WITH
-current_from_snapshot as (
-    {{ current_from_snapshot(
-        snsh_ref = ref('SNSH_ABC_BANK_SECURITY_INFO') ) }}
+WITH current_from_history as (
+    {{
+        current_from_history(
+            history_rel= ref("HIST_ABC_BANK_SECURITY_INFO"),
+            key_column= "SECURITY_HKEY",
+        )
+    }}
 )
 
-SELECT * 
-FROM current_from_snapshot
+SELECT 
+    *
+FROM current_from_history

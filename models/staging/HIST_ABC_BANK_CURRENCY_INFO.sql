68ff670a716efef40ca29ed1221736536073688a
Guilherme Matte <guilhermematte93@gmail.com>
2026-01-22 13:17:42 +0000

    Minor bug fixes

--- models/staging/HIST_ABC_BANK_CURRENCY_INFO.sql
+++ models/staging/HIST_ABC_BANK_CURRENCY_INFO.sql
@@ -1,5 +1,9 @@
-{{ save_history(
+with save_history as (
+    {{ save_history(
     input_rel = ref("STG_ABC_BANK_CURRENCY_INFO"),
     key_column = 'CURRENCY_HKEY',
     diff_column = 'CURRENCY_HDIFF'
 )}}
+)
+
+SELECT * FROM save_history
